68229b71384caed0aefaff512fca7f8e53003f90
FangYuan [方圆] <fangyuan@xiaomi.com>
2015-07-01 20:22:43 +0800

    add intellij short keys doc

---
+++ src/main/java/com/alex/demo/AlexDemo.java
@@ -0,0 +1,22 @@
+package com.alex.demo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by alexfang on 2015/7/1.
+ */
+public class AlexDemo {
+    public static void main(String[] args) {
+        List<String> books =new ArrayList<String>();
+        for (Iterator<String> iterator = books.iterator(); iterator.hasNext(); ) {
+            String next =  iterator.next();
+            
+        }
+        if (books != null) {
+            return;
+        }
+
+    }
+}
